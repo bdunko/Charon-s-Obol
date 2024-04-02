df8a671878ec33929e13fc8b65199366d2432204
bdunko <bdunko@gmail.com>
2024-04-01 23:11:11 -0400

    Make slightly more space

--- ui/charon_textbox.gd
+++ ui/charon_textbox.gd
@@ -13,7 +13,7 @@ func _on_state_changed() -> void:
 		Global.State.AFTER_FLIP:
 			_TEXTBOX.set_text("Payoff...")
 		Global.State.SHOP:
-			_TEXTBOX.set_text("New coins...?")
+			_TEXTBOX.set_text("Buy or sell...?")
 		Global.State.GAME_OVER:
 			var victory = Global.coin_value >= Global.GOAL_COIN_VALUE
 			_TEXTBOX.set_text("What? You win? How??" if victory else "Your soul is mine!")
