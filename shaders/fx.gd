1277cbde2ed47573922ed1cbd1a5a4dfda8b5f67
bdunko <bdunko@gmail.com>
2024-11-16 23:22:54 -0500

    Added auto scanline; added remaining effects to FX

--- shaders/fx.gd
+++ shaders/fx.gd
@@ -4,7 +4,6 @@ extends AnimationPlayer
 signal tween_finished
 
 const _UNIFORM_MOUSE := "MOUSE"
-const OUTLINE_COLOR = Color(20.0/255.0, 16.0/255.0, 19.0/255.0)
 
 enum Uniform {
 	BOOL_USE_EXCLUDE_COLORS, VEC3_EXCLUDE_COLOR1, VEC3_EXCLUDE_COLOR2, VEC3_EXCLUDE_COLOR3, VEC3_EXCLUDE_COLOR4,
@@ -28,10 +27,18 @@ enum Uniform {
 	FLOAT_BRIGHTNESS_CORRECT, FLOAT_CONTRAST_CORRECT, FLOAT_SATURATION_CORRECT,
 	
 	VEC3_SCANLINE_COLOR, FLOAT_SCANLINE_STRENGTH,
-	FLOAT_SCANLINE_LEFT_TO_RIGHT_POSITION,
+	FLOAT_SCANLINE_LEFT_TO_RIGHT_POSITION, 
+	BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_ON, BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_REVERSE,
+	FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_START_TIME, FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_SCAN_DURATION, FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_DELAY,
 	FLOAT_SCANLINE_TOP_TO_BOTTOM_POSITION,
+	BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_ON, BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_REVERSE,
+	FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_START_TIME, FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_SCAN_DURATION, FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_DELAY,
 	FLOAT_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_POSITION,
+	BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_ON, BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_REVERSE,
+	FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_START_TIME, FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_SCAN_DURATION, FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_DELAY,
 	FLOAT_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_POSITION,
+	BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_ON, BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_REVERSE,
+	FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_START_TIME, FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_SCAN_DURATION, FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_DELAY,
 	
 	INT_DISINTEGRATE_SEED, VEC3_DISINTEGRATE_COLOR, FLOAT_DISINTEGRATE_STRENGTH, FLOAT_DISINTEGRATE_ALPHA_BOUND1, FLOAT_DISINTEGRATE_ALPHA_BOUND2,
 	BOOL_DISINTEGRATE_AS_STATIC, 
@@ -69,6 +76,10 @@ const _UNIFORM_TO_STR = {
 	Uniform.VEC3_FLASH_COLOR : "flash_color", Uniform.FLOAT_FLASH_STRENGTH : "flash_strength", Uniform.FLOAT_AUTO_FLASH_SPEED : "auto_flash_speed", Uniform.FLOAT_AUTO_FLASH_BOUND : "auto_flash_bound", Uniform.FLOAT_AUTO_FLASH_START_TIME : "auto_flash_start_time",
 	Uniform.FLOAT_RED_CORRECT : "red_correct", Uniform.FLOAT_GREEN_CORRECT : "green_correct", Uniform.FLOAT_BLUE_CORRECT : "blue_correct", Uniform.FLOAT_GRAY_CORRECT : "gray_correct", Uniform.FLOAT_BRIGHTNESS_CORRECT : "brightness_correct", Uniform.FLOAT_CONTRAST_CORRECT : "contrast_correct", Uniform.FLOAT_SATURATION_CORRECT : "saturation_correct",
 	Uniform.VEC3_SCANLINE_COLOR : "scanline_color", Uniform.FLOAT_SCANLINE_STRENGTH : "scanline_strength", Uniform.FLOAT_SCANLINE_LEFT_TO_RIGHT_POSITION : "scanline_left_to_right_position", Uniform.FLOAT_SCANLINE_TOP_TO_BOTTOM_POSITION : "scanline_top_to_bottom_position", Uniform.FLOAT_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_POSITION : "scanline_topleft_to_bottomright_position", Uniform.FLOAT_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_POSITION : "scanline_topright_to_bottomleft_position",
+	Uniform.BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_ON : "auto_scanline_left_to_right_on", Uniform.BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_REVERSE : "auto_scanline_left_to_right_reverse", Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_START_TIME : "auto_scanline_left_to_right_start_time", Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_SCAN_DURATION : "auto_scanline_left_to_right_scan_duration", Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_DELAY : "auto_scanline_left_to_right_delay",
+	Uniform.BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_ON : "auto_scanline_top_to_bottom_on", Uniform.BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_REVERSE : "auto_scanline_top_to_bottom_reverse", Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_START_TIME : "auto_scanline_top_to_bottom_start_time", Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_SCAN_DURATION : "auto_scanline_top_to_bottom_scan_duration", Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_DELAY : "auto_scanline_top_to_bottom_delay",
+	Uniform.BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_ON : "auto_scanline_topleft_to_bottomright_on", Uniform.BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_REVERSE : "auto_scanline_topleft_to_bottomright_reverse", Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_START_TIME : "auto_scanline_topleft_to_bottomright_start_time", Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_SCAN_DURATION : "auto_scanline_topleft_to_bottomright_scan_duration", Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_DELAY : "auto_scanline_topleft_to_bottomright_delay",
+	Uniform.BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_ON : "auto_scanline_topright_to_bottomleft_on", Uniform.BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_REVERSE : "auto_scanline_topright_to_bottomleft_reverse", Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_START_TIME : "auto_scanline_topright_to_bottomleft_start_time", Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_SCAN_DURATION : "auto_scanline_topright_to_bottomleft_scan_duration", Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_DELAY : "auto_scanline_topright_to_bottomleft_delay",
 	Uniform.INT_DISINTEGRATE_SEED : "disintegrate_seed", Uniform.VEC3_DISINTEGRATE_COLOR : "disintegrate_color",  Uniform.FLOAT_DISINTEGRATE_STRENGTH : "disintegrate_strength", Uniform.FLOAT_DISINTEGRATE_ALPHA_BOUND1 : "disintegrate_alpha_bound1",Uniform.FLOAT_DISINTEGRATE_ALPHA_BOUND2 : "disintegrate_alpha_bound2", Uniform.BOOL_DISINTEGRATE_AS_STATIC : "disintegrate_as_static",  Uniform.FLOAT_AUTO_DISINTEGRATE_RANDOM_SEED_SPEED : "auto_disintegrate_random_seed_speed",
 	Uniform.VEC4_GLOW_COLOR : "glow_color", Uniform.INT_GLOW_THICKNESS : "glow_thickness", Uniform.BOOL_GLOW_DIAGONALS : "glow_diagonals", Uniform.FLOAT_AUTO_GLOW_SPEED : "auto_glow_speed", Uniform.FLOAT_AUTO_GLOW_BOUND : "auto_glow_bound", Uniform.FLOAT_AUTO_GLOW_START_TIME : "auto_glow_start_time", 
 	Uniform.BOOL_FOG_ENABLED : "fog_enabled", Uniform.SAMPLER2D_FOG_NOISE_TEXTURE : "fog_noise_texture", Uniform.FLOAT_FOG_OPACITY : "fog_opacity", Uniform.FLOAT_FOG_DENSITY : "fog_density", Uniform.VEC2_FOG_SPEED : "fog_speed",
@@ -85,6 +96,7 @@ func START_TIME() -> float:
 	return Time.get_ticks_msec()/1000.0
 
 var _active_tweens = {}
+var _outline_color = Color(20.0/255.0, 16.0/255.0, 19.0/255.0)
 
 func _ready() -> void:
 	assert(get_parent() is CanvasItem)
@@ -164,8 +176,16 @@ func _process(_delta) -> void:
 
 
 ## PREBAKED EFFECTS ##
+enum ScanDirection {
+	LEFT_TO_RIGHT, RIGHT_TO_LEFT,
+	BOTTOM_TO_TOP, TOP_TO_BOTTOM,
+	DIAGONAL_TOPLEFT_TO_BOTTOMRIGHT, DIAGONAL_BOTTOMRIGHT_TO_TOPLEFT,
+	DIAGONAL_TOPRIGHT_TO_BOTTOMLEFT, DIAGONAL_BOTTOMLEFT_TO_TOPRIGHT
+}
+
 func flash(color: Color, time: float = 0.1) -> void:
-	assert(time >= 0.0)
+	assert(time >= 0.0, "Time must be non-negative.")
+	
 	set_uniform(Uniform.VEC3_FLASH_COLOR, color)
 	set_uniform(Uniform.FLOAT_FLASH_STRENGTH, 0.0) 
 	set_uniform(Uniform.FLOAT_AUTO_FLASH_START_TIME, START_TIME())
@@ -180,25 +200,24 @@ func slow_flash(color: Color) -> void:
 
 func recolor(i: int, color: Color, with: Color) -> void:
 	assert(i >= 1 and i <= 5, "We only support replacing up to 5 colors.")
-	if i < 1 or i > 5:
-		return
 		
-	# little lookup table just for fanciness
-	var a = [[Uniform.VEC3_REPLACE_COLOR1, Uniform.VEC3_REPLACE_WITH_COLOR1],
+	var recolor_lookup = [[Uniform.VEC3_REPLACE_COLOR1, Uniform.VEC3_REPLACE_WITH_COLOR1],
 		[Uniform.VEC3_REPLACE_COLOR2, Uniform.VEC3_REPLACE_WITH_COLOR2],
 		[Uniform.VEC3_REPLACE_COLOR3, Uniform.VEC3_REPLACE_WITH_COLOR3],
 		[Uniform.VEC3_REPLACE_COLOR4, Uniform.VEC3_REPLACE_WITH_COLOR4],
 		[Uniform.VEC3_REPLACE_COLOR5, Uniform.VEC3_REPLACE_WITH_COLOR5]]
 	
-	set_uniform(a[i][0], color) #VEC3_REPLACE_COLORi
-	set_uniform(a[i][1], with) #VEC3_REPLACE_WITH_COLORi
+	set_uniform(recolor_lookup[i][0], color) #VEC3_REPLACE_COLORi
+	set_uniform(recolor_lookup[i][1], with) #VEC3_REPLACE_WITH_COLORi
 
-func recolor_outline(color: Color) -> void:
-	set_uniform(Uniform.VEC3_REPLACE_COLOR_OUTLINE, OUTLINE_COLOR) # just in case
+func recolor_outline(color: Color, base_outline_color = _outline_color) -> void:
+	_outline_color = base_outline_color # allows a new outline color to be provided if default does not suffice
+	set_uniform(Uniform.VEC3_REPLACE_COLOR_OUTLINE, _outline_color) 
 	set_uniform(Uniform.VEC3_REPLACE_WITH_COLOR_OUTLINE, color)
 
 func tint(color: Color, strength: float) -> void:
 	assert(strength >= 0.0 and strength <= 1.0)
+	
 	set_uniform(Uniform.VEC3_TINT_COLOR, color)
 	set_uniform(Uniform.FLOAT_TINT_STRENGTH, strength)
 
@@ -208,6 +227,7 @@ func clear_tint() -> void:
 func start_glowing(color: Color, speed: float = 0, thickness: int = 1, minimum: float = 0.75) -> void:
 	assert(thickness > 0, "Thickness must be larger than 0.")
 	assert(minimum >= 0.0 and minimum <= 1.0, "Minimum must be between 0 and 1.")
+	
 	set_uniform(Uniform.FLOAT_AUTO_GLOW_START_TIME, START_TIME())
 	set_uniform(Uniform.VEC4_GLOW_COLOR, color)
 	set_uniform(Uniform.INT_GLOW_THICKNESS, thickness)
@@ -225,17 +245,94 @@ func fade_in(time: float) -> void:
 	assert(time >= 0.0)
 	await tween_uniform(Uniform.FLOAT_TRANSPARENCY, 1.0, time)
 
+const _SCAN_LOOKUP = {
+	# direction: [uniform, start, end, [auto_on, auto_reverse, auto_start_time, auto_duration, auto_delay], auto_reverse]
+	ScanDirection.LEFT_TO_RIGHT : [Uniform.FLOAT_SCANLINE_LEFT_TO_RIGHT_POSITION, 0.0, 1.0, [Uniform.BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_ON, Uniform.BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_DELAY], false],
+	ScanDirection.RIGHT_TO_LEFT : [Uniform.FLOAT_SCANLINE_LEFT_TO_RIGHT_POSITION, 1.0, 0.0, [Uniform.BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_ON, Uniform.BOOL_AUTO_SCANLINE_LEFT_TO_RIGHT_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_LEFT_TO_RIGHT_DELAY], true],
+	ScanDirection.TOP_TO_BOTTOM : [Uniform.FLOAT_SCANLINE_TOP_TO_BOTTOM_POSITION, 0.0, 1.0, [Uniform.BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_ON, Uniform.BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_DELAY], false],
+	ScanDirection.BOTTOM_TO_TOP : [Uniform.FLOAT_SCANLINE_TOP_TO_BOTTOM_POSITION, 1.0, 0.0, [Uniform.BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_ON, Uniform.BOOL_AUTO_SCANLINE_TOP_TO_BOTTOM_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_TOP_TO_BOTTOM_DELAY], true],
+	ScanDirection.DIAGONAL_TOPLEFT_TO_BOTTOMRIGHT : [Uniform.FLOAT_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_POSITION, 0.0, 1.0, [Uniform.BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_ON, Uniform.BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_DELAY], false],
+	ScanDirection.DIAGONAL_BOTTOMRIGHT_TO_TOPLEFT : [Uniform.FLOAT_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_POSITION, 1.0, 0.0, [Uniform.BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_ON, Uniform.BOOL_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_TOPLEFT_TO_BOTTOMRIGHT_DELAY], true],
+	ScanDirection.DIAGONAL_TOPRIGHT_TO_BOTTOMLEFT : [Uniform.FLOAT_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_POSITION, 0.0, 1.0, [Uniform.BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_ON, Uniform.BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_DELAY], false],
+	ScanDirection.DIAGONAL_BOTTOMLEFT_TO_TOPRIGHT : [Uniform.FLOAT_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_POSITION, 1.0, 0.0, [Uniform.BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_ON, Uniform.BOOL_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_REVERSE, Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_START_TIME, Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_SCAN_DURATION, Uniform.FLOAT_AUTO_SCANLINE_TOPRIGHT_TO_BOTTOMLEFT_DELAY], true],
+}
+
+func scan(direction: ScanDirection, color: Color, strength: float = 1.0, time: float = 1.0) -> void:
+	assert(strength >= 0 and strength <= 1, "Strength must be between 0 and 1.")
+	assert(time >= 0.0, "Time must be non-negative.")
+	
+	var uniform: Uniform = _SCAN_LOOKUP[direction][0]
+	var start: float = _SCAN_LOOKUP[direction][1]
+	var end: float = _SCAN_LOOKUP[direction][2]
+	
+	set_uniform(Uniform.VEC3_SCANLINE_COLOR, color)
+	set_uniform(Uniform.FLOAT_SCANLINE_STRENGTH, strength)
+	set_uniform(uniform, start)
+	
+	await tween_uniform(uniform, end, time)
+
+func disintegrate(time: float) -> void:
+	assert(time >= 0.0, "Time must be non-negative.")
+	
+	set_uniform(Uniform.FLOAT_DISINTEGRATE_ALPHA_BOUND1, 0.0)
+	set_uniform(Uniform.FLOAT_DISINTEGRATE_ALPHA_BOUND1, 0.0)
+	set_uniform(Uniform.FLOAT_DISINTEGRATE_STRENGTH, 0.0)
+	
+	await tween_uniform(Uniform.FLOAT_DISINTEGRATE_STRENGTH, 1.0, time)
+
+func start_flickering(speed: float, alpha_bound1: float, alpha_bound2: float) -> void:
+	assert(speed >= 0, "Speed must be non-negative.")
+	assert(alpha_bound1 >= 0 and alpha_bound1 <= 1, "Bounds must be between 0 and 1.")
+	assert(alpha_bound2 >= 0 and alpha_bound2 <= 1, "Bounds must be between 0 and 1.")
+	
+	set_uniform(Uniform.FLOAT_TRANSPARENCY, alpha_bound1)
+	set_uniform(Uniform.FLOAT_AUTO_FLICKER_BOUND, alpha_bound2)
+	set_uniform(Uniform.FLOAT_AUTO_FLICKER_SPEED, speed)
+	set_uniform(Uniform.FLOAT_AUTO_FLICKER_START_TIME, START_TIME())
+
+func stop_flickering(ending_alpha: float = 1.0) -> void:
+	assert(ending_alpha >= 0 and ending_alpha <= 1, "Alpha must be between 0 and 1.")
+	
+	set_uniform(Uniform.FLOAT_AUTO_FLICKER_SPEED, 0.0)
+	set_uniform(Uniform.FLOAT_TRANSPARENCY, ending_alpha)
+
+func start_flashing(speed: float, strength_bound1: float, strength_bound2: float) -> void:
+	assert(speed >= 0, "Speed must be non-negative.")
+	assert(strength_bound1 >= 0 and strength_bound1 <= 1, "Bounds must be between 0 and 1.")
+	assert(strength_bound2 >= 0 and strength_bound2 <= 1, "Bounds must be between 0 and 1.")
+	
+	set_uniform(Uniform.FLOAT_FLASH_STRENGTH, strength_bound1)
+	set_uniform(Uniform.FLOAT_AUTO_FLASH_BOUND, strength_bound2)
+	set_uniform(Uniform.FLOAT_AUTO_FLASH_SPEED, speed)
+	set_uniform(Uniform.FLOAT_AUTO_FLASH_START_TIME, START_TIME())
+
+func stop_flashing() -> void:
+	set_uniform(Uniform.FLOAT_AUTO_FLASH_SPEED, 0.0)
+	set_uniform(Uniform.FLOAT_FLASH_STRENGTH, 0.0) # reset flash quickly
+
+func start_scanning(direction: ScanDirection, scan_duration: float, delay: float) -> void:
+	assert(scan_duration >= 0, "Scan duration must be non-negative.")
+	assert(delay >= 0, "Delay must be non-negative.")
+	
+	var uniform_on: Uniform = _SCAN_LOOKUP[direction][3][0]
+	var uniform_reverse: Uniform = _SCAN_LOOKUP[direction][3][1]
+	var uniform_start_time: Uniform = _SCAN_LOOKUP[direction][3][2]
+	var uniform_scan_duration: Uniform = _SCAN_LOOKUP[direction][3][3]
+	var uniform_delay: Uniform = _SCAN_LOOKUP[direction][3][4]
+	var reverse: bool = _SCAN_LOOKUP[direction][4]
+	
+	set_uniform(uniform_on, true)
+	set_uniform(uniform_reverse, reverse)
+	set_uniform(uniform_start_time, START_TIME())
+	set_uniform(uniform_scan_duration, scan_duration)
+	set_uniform(uniform_delay, delay)
+
+func stop_scanning(direction: ScanDirection) -> void: 
+	var uniform_on: Uniform = _SCAN_LOOKUP[direction][3][0]
+	set_uniform(uniform_on, false)
 
 # prebaked effects -
 # - added -
-# scan (pass scan direction as enum)
-# disintegrate
-# start_flickering / stop_flickering <- needs start time
-# start_flashing / stop_flashing
-
-# need to implement into shader to do auto scans, I think we want this
-# start_scanning / stop_scanning
-
 # TODO - search for replace_color (renamed to recolor)
 # TODO - search for outline (renamed to recolor_outline)
 # TODO _ removed clear_replace_color
